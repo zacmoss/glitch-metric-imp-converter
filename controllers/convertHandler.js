f8f46792273ab954aeb3199e51ab552a9f50309f
Glitch (abounding-leopard) <none>
2018-09-21 15:49:54 +0000

    👠📫 Checkpoint ./controllers/convertHandler.js:1744758/1782

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -52,12 +52,36 @@ function ConvertHandler() {
     return result;
   };
   
+  // works
   this.getReturnUnit = function(initUnit) {
     var result;
+    let x = initUnit.toLowerCase();
+    console.log('spellOutUnit - ' + x + ' - should be lowercase');
+    switch(x) {
+      case 'kg':
+        result = 'lbs';
+        break;
+      case 'lbs':
+        result = 'kg';
+        break;
+      case 'mi':
+        result = 'km';
+        break;      
+      case 'km':
+        result = 'mi';
+        break;      
+      case 'gal':
+        result = 'L';
+        break;      
+      case 'l':
+        result = 'gal';
+        break;      
+    }
     
     return result;
   };
 
+  // works
   this.spellOutUnit = function(unit) {
     var result;
     let x = unit.toLowerCase();
@@ -88,9 +112,34 @@ function ConvertHandler() {
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
+    const lToGal = 0.264172;
     const lbsToKg = 0.453592;
+    const kgToLbs = 2.20462;
     const miToKm = 1.60934;
+    const kmToMi = 0.621371;
     var result;
+    
+    let x = initUnit.toLowerCase();
+    switch(x) {
+      case 'kg':
+        result = initNum * kgToLbs;
+        break;
+      case 'lbs':
+        result = initNum * lbsToKg;
+        break;
+      case 'mi':
+        result = initNum * miToKm;
+        break;      
+      case 'km':
+        result = initNum * kmToMi;
+        break;      
+      case 'gal':
+        result = initNum * galToL;
+        break;      
+      case 'l':
+        result = initNum * lToGal;
+        break;      
+    }
     
     return result;
   };
