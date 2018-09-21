4558b0399d49baa475492e1752284a2710a26fcf
Glitch (abounding-leopard) <none>
2018-09-21 15:17:25 +0000

    👱🎲 Checkpoint ./controllers/convertHandler.js:1744758/411

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -12,14 +12,27 @@ function ConvertHandler() {
     var result;
     let array = input.split(/[a-zA-Z]+/);
     let num = array[0]
-    //if (num.includes('/')) num = num.split('/');
-    num = num.split('/');
+    if (num === '') {
+      result = 1;
+    } else if (num.includes('/')) {
+      num = num.split('/');
+      result = num[0] / num[1];
+    } else {
+      result = num;
+    }
+    //num = num.split('/');
     
     return result;
   };
   
   this.getUnit = function(input) {
     var result;
+    let myArray = input.split(/([0-9]+)/);
+    if (myArray[0] === '') {
+      //is number
+    } else {
+      //is not number
+      // check for appropriate unit
     
     return result;
   };
